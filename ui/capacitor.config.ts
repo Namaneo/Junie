b0b18007fc335bcd431adbb3df6bbf73554f132e
Julien Loir <mini.jul.jl@gmail.com>
2021-11-19 15:04:56 +0100

    Update capacitor.config.ts

--- ui/capacitor.config.ts
+++ ui/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'JunieUI',
+  appName: 'Junie',
   webDir: 'build',
   bundledWebRuntime: false
 };
