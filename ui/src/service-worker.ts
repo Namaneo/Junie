186726343f42d7d9dfaf491eaef8ea6a728e73fb
Julien Loir <mini.jul.jl@gmail.com>
2021-11-28 15:05:05 +0100

    Add clients claim to serve cache at first visit

--- ui/src/service-worker.ts
+++ ui/src/service-worker.ts
@@ -1,11 +1,13 @@
 /// <reference lib="webworker" />
 
-import { skipWaiting } from 'workbox-core';
+import { clientsClaim, skipWaiting } from 'workbox-core';
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
+
+clientsClaim();
 
 precacheAndRoute(self.__WB_MANIFEST);
 
