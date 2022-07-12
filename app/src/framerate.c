0bfbfef433ab111a41fe7261e51ef0241790ef8f
Julien Loir <mini.jul.jl@gmail.com>
2022-07-12 22:27:09 +0200

    Framerate was broken... Fixed.

--- app/src/framerate.c
+++ app/src/framerate.c
@@ -5,27 +5,30 @@
 static struct {
     MTY_Time before_run;
     MTY_Time after_run;
-    float last_diff;
     float remaining;
-    bool throttling;
 } CTX;
 
 uint32_t JUN_FramerateGetFactor()
 {
-    CTX.before_run = MTY_GetTime();
-    CTX.last_diff = MTY_TimeDiff(CTX.after_run, CTX.before_run);
+    MTY_Time before_run = MTY_GetTime();
 
-	float framerate = 1000.0 / CTX.last_diff;
+    float total_loop = MTY_TimeDiff(CTX.before_run, before_run);
+    float time_run = MTY_TimeDiff(CTX.before_run, CTX.after_run);
+    float time_idle = MTY_TimeDiff(CTX.after_run, before_run);
+
+    CTX.before_run = before_run;
+
+    bool throttling = time_run > time_idle;
+	float framerate = 1000.0 / total_loop;
 
 	CTX.remaining += 60.0f / framerate;
 	uint32_t pending = (uint32_t) CTX.remaining;
 	CTX.remaining -= (float) pending;
 
-	return pending <= 20 && !CTX.throttling ? pending : 1;
+	return pending <= 20 && !throttling ? pending : 1;
 }
 
 void JUN_FramerateHasRunned()
 {
     CTX.after_run = MTY_GetTime();
-	CTX.throttling = MTY_TimeDiff(CTX.before_run, CTX.after_run) > CTX.last_diff;
 }
